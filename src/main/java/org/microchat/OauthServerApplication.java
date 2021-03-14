92ac440932e4debd0becb68d8dceee60b403f722
zyzbeckham <zyzbeckham@gmail.com>
2021-03-14 22:53:42 +0800

    build and push

--- src/main/java/org/microchat/OauthServerApplication.java
+++ src/main/java/org/microchat/OauthServerApplication.java
@@ -1,12 +1,10 @@
 package org.microchat;
 
 import lombok.Data;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
